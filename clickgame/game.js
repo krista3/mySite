687b56ab96719b7f4a2ae98ffdb07a70e88a6f60
Sebastian <ksebastian@wpi.edu>
2023-07-31 10:30:20 -0400

    mon morning changes

--- clickgame/game.js
+++ clickgame/game.js
@@ -1,3 +1,5 @@
+let goatCost = 15000;
+
 function buy(store) {
     let cost = parseInt(store.getAttribute("cost"));
     let bank = parseInt(score.innerHTML);
@@ -7,21 +9,29 @@ function buy(store) {
 
     changeScore(-1 * cost);
 
+
     let widget = document.createElement("div");
     widget.className = "widget";
     fillWidget(store, widget);
+
+    if (widget.getAttribute("name")=="G.O.A.T.") {
+        goat(widget);
+        return;
+    }
 
     widget.onclick = () => {
         harvest(widget);
     }
 
     document.getElementById("widget-container").appendChild(widget);
-
+    if (widget.getAttribute("auto") == 'true') {
+        harvest(widget);
+    }
 }
 
 function harvest(widget) {
     // Only run if currently not harvesting
-    if (widget.getAttribute("harvesting")) return;
+    if (widget.hasAttribute("harvesting")) return;
 
     // Set harvesting flag
     widget.setAttribute("harvesting", "");
@@ -39,6 +49,7 @@ function harvest(widget) {
         if (widget.getAttribute("auto") == 'true') {
             changeScore(widget.getAttribute("reap"));
             showPoint(widget);
+            harvest(widget);
         }
     }, parseFloat(widget.getAttribute("cooldown")) * 1000);
 }
@@ -70,4 +81,43 @@ function showPoint(widget) {
         widget.removeChild(number);
     }
     widget.appendChild(number);
+}
+
+function goat(widget) {
+    changeScore(parseInt(score.innerHTML));s
+    //console.log(widget.getAttribute("cost"));
+
+    goatCost+=goatCost;
+    widget.setAttribute("cost", goatCost);
+    //console.log(goatCost);
+    // for (let store of stores) {
+    //     if (store.getAttribute("name") == "G.O.A.T.") {
+    //         store.setAttribute("cost", goatCost);
+    //     }
+    // }
+
+    // for (let store of stores) {
+    //     let cost = parseInt(store.getAttribute("cost"));
+    //     let bank = parseInt(score.innerHTML);
+
+    //     // Display overlay if broke
+    //     if (cost <= bank) {
+    //         store.removeAttribute("broke");
+    //     } else {
+    //         store.setAttribute("broke", "");
+    //     }
+
+    //     if (store.getAttribute("name" == "G.O.A.T.")) {
+            
+    //         console.log("it ran");
+    //         let cost = parseInt(widget.getAttribute("cost"));    
+    //         console.log(cost);
+    //         let newCost = cost * 2;
+    //         widget.setAttribute("cost", newCost);
+    //     }
+    // }
+
+    // // let cost = parseInt(widget.getAttribute("cost"));    
+    // // let newCost = cost * 2;
+    // // widget.setAttribute("cost", newCost);
 }
